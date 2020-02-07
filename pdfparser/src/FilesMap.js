84b65c6f5969162007db5948af3b035a04ceaa76
gusflopes <gusflopes86@gmail.com>
2020-02-06 22:15:03 -0400

    feat: Folders and Files scanner

--- pdfparser/src/FilesMap.js
+++ pdfparser/src/FilesMap.js
@@ -1,6 +1,30 @@
 //requiring path and fs modules
 const path = require("path");
 const fs = require("fs");
+
+function readFiles(nextPath) {
+  const directoryPath = nextPath;
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function(err, files) {
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  //listing all files using forEach
+  files.forEach(function(file) {
+    // Do whatever you want to do with the file
+    if (!path.extname(file) && file !== ".DS_Store") {
+      console.log(`${file} is a Directory`)
+      const newDirectoryPath = path.join(directoryPath, file);
+      readFiles(newDirectoryPath);
+    } else {
+    console.log(file, "in", directoryPath);
+    }
+  });
+});
+
+}
+
 //joining path of directory
 const directoryPath = path.join(__dirname, "..", "samples");
 //passsing directoryPath and callback function
@@ -12,6 +36,12 @@ fs.readdir(directoryPath, function(err, files) {
   //listing all files using forEach
   files.forEach(function(file) {
     // Do whatever you want to do with the file
+    if (!path.extname(file) && file !== ".DS_Store") {
+      console.log(`${file} is a Directory`)
+      const newDirectoryPath = path.join(directoryPath, file);
+      readFiles(newDirectoryPath);
+    } /* else {
     console.log(file);
+    } */
   });
 });
