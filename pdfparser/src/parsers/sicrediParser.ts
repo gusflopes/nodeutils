47e9db4d89b688a68a0b81cbcd30ac1e68a5a87a
gusflopes <gusflopes86@gmail.com>
2020-02-08 10:50:25 -0400

    feat: Sicredi Parser added mutiple layouts

--- pdfparser/src/parsers/sicrediParser.ts
+++ pdfparser/src/parsers/sicrediParser.ts
@@ -4,15 +4,19 @@ import pdfCleaner from './pdfCleaner';
 import stringParser from './stringParser';
 import regexParser from './regexParser';
 
-// let file = fs.readFileSync(`./samples/Sicredi/input.pdf`);
+/**
+ * TODO
+ * [ ] Transferência Sicredi Agendado: salvar arquivo e ajustar os parameters;
+ * [ ] 
+ */
+
 interface Parameters {
   field: String,
   regexMethod: Boolean,
   firstIndex: String,
   secondIndex: RegExp,
 }
-// Comprovante Transferencia Sicredi
-const parameters = [
+const parameterSicrediTedOutratitularidadeConfirmado = [
   {
     field: 'data',
     regexMethod: true,
@@ -39,9 +43,157 @@ const parameters = [
     firstIndex: 'Identificador: ',
     secondIndex: /Motivo Transferência/gm,
 }];
+const parameterSicrediTransferenciaSicrediConfirmado = [{
+  field: 'data',
+  regexMethod: true,
+  firstIndex: null,
+  secondIndex: /\d{2}\/\d{2}\/\d{4}/,
+}, {
+  field: 'valor',
+  regexMethod: false,
+  firstIndex: 'Motivo da Transferência: ',
+  secondIndex: /Valor Transferido \(R\$\):/gm,
+}, {
+  field: 'favorecido',
+  regexMethod: false,
+  firstIndex: 'Origem dos Recursos: ',
+  secondIndex:  /Favorecido:/gm,
+}, {
+  // Transferência entre contas do mesmo banco não tem cpfCnpj, usando ag e conta
+  field: 'agencia',
+  regexMethod: false,
+  firstIndex: 'Conta Destino: ',
+  secondIndex:  /Cooperativa Destino:/gm,
+}, {
+  field: 'conta',
+  regexMethod: false,
+  firstIndex: 'Favorecido: ',
+  secondIndex: /Conta Destino:/gm,
+}, {
+  field: 'observacoes',
+  regexMethod: false,
+  firstIndex: 'Autenticação Eletrônica: ',
+  secondIndex: /Motivo da Transferência/gm,
+}];
+const parametersSicrediBoleto = [{
+  field: 'data',
+  regexMethod: true,
+  firstIndex: null,
+  secondIndex: /\d{2}\/\d{2}\/\d{4}/,
+}, {
+  field: 'valor',
+  regexMethod: false,
+  firstIndex: 'Descrição do Pagamento: ',
+  secondIndex: /Valor Pago \(R\$\):/gm,
+}, {
+  field: 'favorecido',
+  regexMethod: false,
+  firstIndex: 'Nome Fantasia do Beneficiário: ',
+  secondIndex:  /Razão Social do Beneficiário:/gm,
+}, {
+  field: 'cpfCnpj',
+  regexMethod: false,
+  firstIndex: 'Nome do Pagador: ',
+  secondIndex:  /CPF\/CNPJ do Beneficiário:/gm,
+}, {
+  field: 'observacoes',
+  regexMethod: false,
+  firstIndex: 'Autenticação Eletrônica: ',
+  secondIndex: /Descrição do Pagamento:/gm,
+}];
+const parameterSicrediTedOutratitularidadeAgendado = [
+  {
+    field: 'data',
+    regexMethod: true,
+    firstIndex: null,
+    secondIndex: /\d{2}\/\d{2}\/\d{4}/,
+  }, {
+    field: 'valor',
+    regexMethod: false,
+    firstIndex: 'Finalidade: ',
+    secondIndex: /Valor a Transferir \(R\$\):/gm,
+  }, {
+    field: 'cpfCnpj',
+    regexMethod: false,
+    firstIndex: 'Data de Criação: ',
+    secondIndex: /CPF\/CNPJ/gm,
+  }, {
+    field: 'favorecido',
+    regexMethod: false,
+    firstIndex: 'CPF/CNPJ: ',
+    secondIndex:  /Favorecido:/gm,
+  }, {
+    field: 'observacoes',
+    regexMethod: false,
+    firstIndex: 'Identificador: ',
+    secondIndex: /Motivo Transferência/gm,
+}];
+const parameterSicrediTransferenciaSicrediAgendado = [{
+  field: 'data',
+  regexMethod: true,
+  firstIndex: null,
+  secondIndex: /\d{2}\/\d{2}\/\d{4}/,
+}, {
+  field: 'valor',
+  regexMethod: false,
+  firstIndex: 'Motivo da Transferência: ',
+  secondIndex: /Valor Transferido \(R\$\):/gm,
+}, {
+  field: 'favorecido',
+  regexMethod: false,
+  firstIndex: 'Origem dos Recursos: ',
+  secondIndex:  /Favorecido:/gm,
+}, {
+  // Transferência entre contas do mesmo banco não tem cpfCnpj, usando ag e conta
+  field: 'agencia',
+  regexMethod: false,
+  firstIndex: 'Conta Destino: ',
+  secondIndex:  /Cooperativa Destino:/gm,
+}, {
+  field: 'conta',
+  regexMethod: false,
+  firstIndex: 'Favorecido: ',
+  secondIndex: /Conta Destino:/gm,
+}, {
+  field: 'observacoes',
+  regexMethod: false,
+  firstIndex: 'Autenticação Eletrônica: ',
+  secondIndex: /Motivo da Transferência/gm,
+}];
+
+  /*******************************
+   * PARSERS
+   *******************************/
+  // let parser = parameterSicrediTedOutratitularidadeConfirmado;
+  // let parser = parameterSicrediTransferenciaSicrediConfirmado;
+  // let parser = parameterSicrediTedOutratitularidadeAgendado;
+  // let parser = parameterSicrediTransferenciaSicrediAgendado; 
+  let parser = parametersSicrediBoleto;
+
+  /*******************************
+   * FILES
+   *******************************/
+  // ###### ATENÇÃO AQUI #########
+  // Salvar arquivos na segunda-feira !!!!!!!!!!!!!!!!!!!!!!!
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-ted-outratitularidade-confirmado-1.pdf`);
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-ted-outratitularidade-confirmado-2.pdf)
+
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-ted-outratitularidade-agendado-1.pdf`);
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-ted-outratitularidade-agendado-2.pdf`);
+
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-transferencia-sicredi-confirmado-1.pdf`);
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-transferencia-sicredi-confirmado-2.pdf`);
+
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-transferencia-sicredi-agendado-1.pdf`);
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-transferencia-sicredi-agendado-2.pdf`);
+
+  let file = fs.readFileSync(`./samples/Sicredi/sicredi-boletos-1.pdf`);
+  // let file = fs.readFileSync(`./samples/Sicredi/sicredi-boletos-2.pdf`);
+
+
+// let file = fs.readFileSync(`./samples/Sicredi/input.pdf`);
 
 async function sicrediTransferenciaParser(input: String, fields: Array<Parameters>) {
-  let response = [];
   const resultPromises = Promise.all(await fields.map(async f => {
     if (f.regexMethod) {
       return await regexParser(f.secondIndex, input);
@@ -54,51 +206,47 @@ async function sicrediTransferenciaParser(input: String, fields: Array<Parameter
 }
 
 
-let file = fs.readFileSync(`./samples/Sicredi/input-1.pdf`);
-
 
 
 // Format PDF
 async function generalParser(file) {
+  // Extrair o texto do PDF
   const pdfText = await pdfReader(file).then(data => {return data})
   
+  // Sanitização dos dados extraídos
   const pdfCleansed = await pdfCleaner(pdfText);
   
-  // Identifica qual parser precisa chamar e chama
-  // No case é Sicredi/Comprovante de Transferência
+  // Comentar aqui para estudar estrutura
+  // return console.log(pdfCleansed);
+
+  // Desenvolver
+  // Identificar qual tipo de documento e qual parser precisa ser chamado
+  const parameters = parser
+
+  // Neste caso, estou passando Sicredi/Comprovante de Transferência Agendado como default
   const pdfParsed = await sicrediTransferenciaParser(pdfCleansed, parameters);
   
-  console.log('pdfParsed', pdfParsed);
+  console.log('pdfParsed is an Array: ', pdfParsed);
   const resultObj = {};
   let index = 0;
   for (const obj of parameters) {
-      const objValues = Object.values(obj);
+      // const objValues = Object.values(obj);
       resultObj[obj.field] = pdfParsed[index];
       index++;
   }
-console.log(resultObj);
-  /*
-  let response = {}
+  console.log('resultObj is an Object:')
+  // console.log(resultObj);
   
-
-  const newData = [];
-  const result = {}
-    pdfParsed.map((value,index) => {
-      result[parameters[index].field] = value;
-      newData.push(result);
-      console.log('index', index, 'value:', value)
-    })
-
-  console.log('response', response);
-  console.log('newData', newData);
-  */
-  return pdfParsed;
+  return resultObj;
 
 }
 
-
-generalParser(file);
-
+// Chamada assíncrona da função para simular o comportamento real
+// O método vai ser chamado por uma função asíncrona e, quando resolver, salvar algo no banco 
+async function asyncCall() {
+  console.log(await generalParser(file));
+}
+asyncCall();
 
 /*** Dados a Extrair
  * output = {
